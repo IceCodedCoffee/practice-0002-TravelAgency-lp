8d29fac18c1220c7db2ea4fba1d2e14b4ef8b16d
wolfa <wolf.alexander.development@gmail.com>
2024-06-03 08:20:41 +0200

    refactor styling

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const showcase = document.querySelector('.showcase');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     showcase.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+
 })
